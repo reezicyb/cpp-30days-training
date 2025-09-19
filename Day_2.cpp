8f139a692464d9d3241a62641e857d7b006bb40e
reezicyb <jones.estrada3@example.com>
2025-09-20 01:54:42 +0800

    Day 2

--- Day_2.cpp
+++ Day_2.cpp
@@ -4,3 +4,22 @@ Task: Learn variable declaration, types, and printing values.
 */
 
 // Add your code below...
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string str = "Hell Yeah!";
+    int age = 2006;
+    double weight = 72.22;
+    bool isStudent = true; 
+    char letter = 'R';
+
+    cout << str << endl;
+    cout << age << endl;
+    cout << weight << endl;
+    cout << isStudent << endl;
+    cout << letter << endl;
+
+    return 0;
+}
